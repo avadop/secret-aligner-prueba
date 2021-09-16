61f5747eeef9118c184050b4ed37282219961968
Andrea del Vado Puell <andrea.vado.puell@gmail.com>
2021-09-16 22:18:12 +0200

    guardar nuevo paciente

--- src/app/components/nuevo-paciente/nuevo-paciente.component.ts
+++ src/app/components/nuevo-paciente/nuevo-paciente.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { PacienteService } from 'src/app/services/paciente.service';
+import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-nuevo-paciente',
@@ -8,40 +7,24 @@ import { PacienteService } from 'src/app/services/paciente.service';
   styleUrls: ['./nuevo-paciente.component.scss']
 })
 export class NuevoPacienteComponent implements OnInit {
-  formularioNuevoPaciente: FormGroup;
+  @Input() formularioPaciente: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private pacienteService: PacienteService) { }
+  @Output() accionesModal: EventEmitter<string> = new EventEmitter();
 
-  ngOnInit(): void {
-    this.crearFormulario();
-  }
+  constructor() { }
 
-  crearFormulario() {
-    this.formularioNuevoPaciente = this.formBuilder.group({
-      id: Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substr(0, 13),
-      datos_paciente: this.formBuilder.group({
-        nombre: '',
-        apellidos: '',
-        fecha_nacimiento: '',
-        sexo: '',
-      }),
-      ficha_dental: this.formBuilder.group({
-        clinica: '',
-        otros_datos: this.formBuilder.group({
-          recorte_alineadores: '',
-          secretretainer: false
-        })
-      })
-    });
-  }
+  ngOnInit(): void { }
 
   guardarPaciente(){
-    this.pacienteService.crearPaciente(this.formularioNuevoPaciente.value).subscribe();
-    this.limpiarDatos();
+    this.accionesModal.emit("guardar");
   }
 
   limpiarDatos(){
-    this.crearFormulario();
+    this.accionesModal.emit("limpiar");
+  }
+
+  cerrar(){
+    this.accionesModal.emit("cancelar");
   }
 
 }
--- src/app/components/pagina-principal/pagina-principal.component.ts
+++ src/app/components/pagina-principal/pagina-principal.component.ts
@@ -1,6 +1,6 @@
 import { newArray } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ResumenPaciente } from 'src/app/interfaces/resumen_paciente.type';
 import { PacienteService } from 'src/app/services/paciente.service';
 
@@ -12,24 +12,62 @@ import { PacienteService } from 'src/app/services/paciente.service';
 export class PaginaPrincipalComponent implements OnInit {
   resumenPacientes: Array<ResumenPaciente> = new Array();
   resumenPacientesFiltrados: Array<ResumenPaciente> = new Array();
-  valorBusqueda:FormControl = new FormControl('');
-  
-  constructor(private pacienteService: PacienteService) {}
+  valorBusqueda: FormControl = new FormControl('');
+  nuevoPacienteModal: boolean = false;
+  formularioNuevoPaciente: FormGroup;
 
-  ngOnInit():void {
+  constructor(private pacienteService: PacienteService, private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
     this.pacienteService.getResumenTodosPacientes().subscribe(respuesta => {
       this.resumenPacientes = respuesta;
       this.resumenPacientesFiltrados = respuesta;
-    })
+    });
+    this.crearFormulario();
   }
 
-  realizarBusqueda(){
-    if(this.valorBusqueda.value.length >= 3){
+  realizarBusqueda() {
+    if (this.valorBusqueda.value.length >= 3) {
       this.resumenPacientesFiltrados = this.resumenPacientes.filter((resumen: ResumenPaciente) => resumen.nombre.toLocaleLowerCase().includes(this.valorBusqueda.value.toLowerCase()));
     } else {
       this.resumenPacientesFiltrados = this.resumenPacientes;
     }
   }
+
+  abrirModalNuevoPaciente() {
+    this.nuevoPacienteModal = true;
+  }
+
+  capturarAccionesModal(evento: string) {
+    if (evento === "cancelar") {
+      this.nuevoPacienteModal = false;
+    } else {
+      if (evento === "guardar") {
+        this.pacienteService.crearPaciente(this.formularioNuevoPaciente.value).subscribe();
+      }
+      this.crearFormulario();
+    }
+  }
+
+  crearFormulario() {
+    this.formularioNuevoPaciente = this.formBuilder.group({
+      id: Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substr(0, 13),
+      datos_paciente: this.formBuilder.group({
+        nombre: '',
+        apellidos: '',
+        fecha_nacimiento: '',
+        sexo: '',
+      }),
+      ficha_dental: this.formBuilder.group({
+        clinica: '',
+        otros_datos: this.formBuilder.group({
+          recorte_alineadores: '',
+          secretretainer: false
+        })
+      })
+    });
+  }
+
 
 
 }
