ff8d343ccde055a5a5119bef1ca96fed26cf4ab7
Andrea del Vado Puell <andrea.vado.puell@gmail.com>
2021-09-16 20:47:48 +0200

    nuevo paciente modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { TablaPacientesComponent } from './components/tabla-pacientes/tabla-paci
 import { TarjetaPacienteComponent } from './components/tarjeta-paciente/tarjeta-paciente.component';
 import { PaginaPrincipalComponent } from './components/pagina-principal/pagina-principal.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NuevoPacienteComponent } from './components/nuevo-paciente/nuevo-paciente.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     ListadoPacientesComponent,
     TablaPacientesComponent,
     TarjetaPacienteComponent,
-    PaginaPrincipalComponent
+    PaginaPrincipalComponent,
+    NuevoPacienteComponent
   ],
   imports: [
     BrowserModule,
